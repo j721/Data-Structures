eb3fed6e38622777e62ea3d0bb57bafa24d07a96
Julia Vu <juliadvu@gmail.com>
2020-06-08 18:39:00 -0500

    test for queue linked list implementation passing

--- queue/queue.py
+++ queue/queue.py
@@ -16,37 +16,41 @@ Stretch: What if you could only use instances of your Stack class to implement t
 
 
 #implement with an array as underlying storage structure 
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size +=1
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if self.size !=0:
+#             self.size -=1
+#             return self.storage.pop(0)
+
+
+
+from singly_linked_list import LinkedList
+from singly_linked_list import Node
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
     
     def __len__(self):
-        return self.size
+       return self.size
 
     def enqueue(self, value):
         self.size +=1
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if self.size !=0:
             self.size -=1
-            return self.storage.pop(0)
-
-
-
-# from singly_linked_list import LinkedList
-
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         # self.storage = ?
-    
-#     def __len__(self):
-#         pass
-
-#     def enqueue(self, value):
-#         pass
-
-#     def dequeue(self):
-#         pass
+            return self.storage.remove_head()
