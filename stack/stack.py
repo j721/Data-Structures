abbbcdbdfd393dc07ee313d806cd67a6f2edb63e
Julia Vu <juliadvu@gmail.com>
2020-06-08 16:46:11 -0500

    worked on stack with linked list implementation

--- stack/stack.py
+++ stack/stack.py
@@ -13,43 +13,47 @@ return elements in Last In First Out order.
 """
 
 #array as underlying storage structure 
-class Stack:
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def push(self, value):
+#         self.size +=1
+#         return self.storage.append(value)
+
+#     def pop(self):
+#         if self.size !=0:
+#             self.size -= 1
+#             return self.storage.pop()
+
+
+
+#Linked list as underlying storage structure 
+
+from singly_linked_list import LinkedList
+
+# import sys
+# sys.path.append('../singly_linked_list')
+# from singly_linked_list import singly_linked_list
+
+class Stack():
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def push(self, value):
         self.size +=1
-        return self.storage.append(value)
+        return self.storage.add_to_tail(value)
 
     def pop(self):
-        if self.size !=0:
-            self.size -= 1
-            return self.storage.pop()
-
-
-
-#Linked list as underlying storage structure 
-
-# from singly_linked_list import LinkedList
-
-# import sys
-# sys.path.append('../singly_linked_list')
-# from singly_linked_list import singly_linked_list
-
-# class Stack():
-#     def __init__(self):
-#         self.size = 0
-#         # self.storage = ?
-
-#     def __len__(self):
-#         pass
-
-#     def push(self, value):
-#         pass
-
-#     def pop(self):
-#         pass
+        if self.size !=-0:
+            self.size -=1
+            return self.storage.remove_tail()
