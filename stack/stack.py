c603d1586c197251901bcfd4edf67266411e2cee
Julia Vu <juliadvu@gmail.com>
2020-06-08 16:39:12 -0500

    test for stack with array as underlying structure passing

--- stack/stack.py
+++ stack/stack.py
@@ -16,20 +16,29 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size +=1
+        return self.storage.append(value)
 
     def pop(self):
-        pass
+        if self.size !=0:
+            self.size -= 1
+            return self.storage.pop()
 
 
+
+#Linked list as underlying storage structure 
 
 # from singly_linked_list import LinkedList
+
+# import sys
+# sys.path.append('../singly_linked_list')
+# from singly_linked_list import singly_linked_list
 
 # class Stack():
 #     def __init__(self):
