eacbe0a0e6a698753b3aedb70781c085aa05f2ca
Julia Vu <juliadvu@gmail.com>
2020-06-09 21:13:34 -0500

    delete method working, and move_to_front, move_to_end tests are now passing as well

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -111,17 +111,40 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        # if node is self.head:
+        #     return
+        # else:
+        #     value = node.value
+        #     self.delete(node)
+        #     self.add_to_tail(value)
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
-        pass
+        self.length -=1
+
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+        
         
     """Returns the highest value currently in the list"""
     def get_max(self):
